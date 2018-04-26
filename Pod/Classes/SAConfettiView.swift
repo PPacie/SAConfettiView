d4d51c5325aa609009b5178fac52d6ca4858bf32
Pablo Paciello <ppacie@gmail.com>
2018-04-26 16:58:16 +0200

    Update SAConfettiView.swift
    add compatibility to Objective-C

--- Pod/Classes/SAConfettiView.swift
+++ Pod/Classes/SAConfettiView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import QuartzCore
 
-public class SAConfettiView: UIView {
+@objc public class SAConfettiView: UIView {
 
     public enum ConfettiType {
         case Confetti
@@ -46,7 +46,7 @@ public class SAConfettiView: UIView {
         active = false
     }
 
-    public func startConfetti() {
+    @objc public func startConfetti() {
         emitter = CAEmitterLayer()
 
         emitter.emitterPosition = CGPoint(x: frame.size.width / 2.0, y: 0)
@@ -63,7 +63,7 @@ public class SAConfettiView: UIView {
         active = true
     }
 
-    public func stopConfetti() {
+    @objc public func stopConfetti() {
         emitter?.birthRate = 0
         active = false
     }
